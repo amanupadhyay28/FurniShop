3b0ac8560cf24f471c24b4846074725c2031a894
Patrisson <aman2833617@gmail.com>
2024-07-07 15:12:14 +0530

    assets and project setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='w-full max-w-[1440px] mx-auto bg-white'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
