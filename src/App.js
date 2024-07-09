ae2e255068a82d8e332c9a9e5f674719043a01f7
Patrisson <aman2833617@gmail.com>
2024-07-10 00:31:05 +0530

    newletter section done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import Hero from './components/Hero'
-import Features from './components/Features'
-import NewItems from './components/NewItems'
-import FeaturesSecond from './components/FeaturesSecond'
-import Products from './components/Products'
-import Testimonial from './components/Testimonial'
+import React from "react";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import NewItems from "./components/NewItems";
+import FeaturesSecond from "./components/FeaturesSecond";
+import Products from "./components/Products";
+import Testimonial from "./components/Testimonial";
+import Newsletter from "./components/Newsletter";
 const App = () => {
   return (
-    <div className='w-full max-w-[1440px] mx-auto bg-white'>
-      <Hero/>
-      <Features/>
-      <NewItems/>
-      <FeaturesSecond/>
-      <Products/>
-      <Testimonial/>
+    <div className="w-full max-w-[1440px] mx-auto bg-white">
+      <Hero />
+      <Features />
+      <NewItems />
+      <FeaturesSecond />
+      <Products />
+      <Testimonial />
+      <Newsletter />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
